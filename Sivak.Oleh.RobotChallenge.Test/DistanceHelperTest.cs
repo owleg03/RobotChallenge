a62820fb8a612a435f15a9d749833dbe27fa2356
Oleh Sivak <olegsivak.2003@gmail.com>
2022-08-22 17:49:19 +0300

    Add a simple unit test

---
+++ Sivak.Oleh.RobotChallenge.Test/DistanceHelperTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using Robot.Common;
+using FluentAssertions;
+
+namespace Sivak.Oleh.RobotChallenge.Test
+{
+    public class Tests
+    {
+        [Test]
+        public void FindDistance_NormalValues_ReturnsDistance()
+        {
+            // Arrange
+            Position position1 = new (1, 1);
+            Position position2 = new (2, 2);
+            int expectedDistance = 2;
+
+            // Act
+            int distance = DistanceHelper.FindDistance(position1, position2);
+
+            // Assert
+            distance.Should().Be(expectedDistance);
+        }
+    }
+}
